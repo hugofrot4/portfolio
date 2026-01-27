cb73e9c19825622490393474bc3429c91610c418
Hugo Frota <herbtyh@gmail.com>
2026-01-27 15:27:55 -0300

    feat: adicionar menu desktop

--- components/layout/MenuContent.tsx
+++ components/layout/MenuContent.tsx
@@ -1,18 +1,18 @@
 export default function MenuContent() {
     return (
-        <div className="p-4 font-display font-bold uppercase text-base">
-            <ul className="flex flex-col items-center gap-8">
+        <div className="p-4 font-header font-bold uppercase text-sm">
+            <ul className="flex flex-col items-center gap-8 md:flex-row">
                 <li>
-                    <a href="">Projetos</a>
+                    <a href="#" className="hover:text-primary hover:underline decoration-2 transition-all duration-400">Projetos</a>
                 </li>
                 <li>
-                    <a href="">Skills</a>
+                    <a href="#" className="hover:text-primary hover:underline decoration-2 transition-all duration-400">Skills</a>
                 </li>
                 <li>
-                    <a href="">Sobre</a>
+                    <a href="#" className="hover:text-primary hover:underline decoration-2 transition-all duration-400">Sobre</a>
                 </li>
                 <li>
-                    <a href="" className="p-4 rounded border-2 border-foreground flex items-center w-max font-display leading-1 ">Contato</a>
+                    <a href="#" className="px-4 py-2 rounded border-2 border-foreground flex items-center w-max font-header text-xs hover:bg-primary hover:border-primary hover:text-background transition-all duration-400">Contato</a>
                 </li>
             </ul>
         </div>
--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -16,23 +16,28 @@ export default function Navbar() {
             <nav className="border-b-2 border-foreground fixed top-0 left-0 right-0 bg-background/95 backdrop-blur-sm font-header z-20">
                 <Container>
                     <div className="flex items-center w-full justify-between py-4">
-                    <div className="flex items-center">
-                        <div className="flex flex-col font-bold uppercase tracking-widest text-xs pr-2 md:pr-6 border-r border-foreground/20">
-                        <span className="text-primary">Vol. 00</span>
-                        <span>Janeiro, 2026</span>
+                        <div className="flex items-center">
+                            <div className="flex flex-col font-bold uppercase tracking-widest text-xs pr-2 md:pr-6 border-r border-foreground/20">
+                                <span className="text-primary">Vol. 00</span>
+                                <span>Janeiro, 2026</span>
+                            </div>
+                            <div className="flex items-center pl-2 md:pl-6 gap-2">
+                                <Newspaper className="md:size-7"/>
+                                <a href="#">
+                                    <h1 className="uppercase font-bold font-display text-lg md:text-4xl md:pb-1">The Portfolio</h1>
+                                </a>
+                            </div>
                         </div>
-                        <div className="flex items-center pl-2 md:pl-6 gap-2">
-                        <Newspaper />
-                        <h1 className="uppercase font-bold font-display text-lg">The Portfolio</h1>
+                        <div className="hidden md:block">
+                            <MenuContent />
                         </div>
-                    </div>
-                    <div onClick={handleMenuMobile} className="p-2 border-2 border-foreground">
-                        {menuMobileOpen ? <X /> : <Menu />}
-                    </div>
+                        <div onClick={handleMenuMobile} className="p-2 border-2 border-foreground md:hidden">
+                            {menuMobileOpen ? <X /> : <Menu />}
+                        </div>
                     </div>
                 </Container>
             </nav>
-            <div className={`fixed top-0 left-0 right-0 bottom-0 bg-background pt-19 border-l-2 border-foreground z-10 duration-400 ${menuMobileOpen ? 'translate-x-0' : 'translate-x-full'}`}>
+            <div className={`fixed top-0 left-0 right-0 bottom-0 bg-background pt-19 border-l-2 border-foreground z-10 duration-400 md:hidden ${menuMobileOpen ? 'translate-x-0' : 'translate-x-full'}`}>
                 <MenuContent />
             </div>
         </>
